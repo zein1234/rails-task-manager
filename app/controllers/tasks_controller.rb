333e7c4bb42dc9982af383a26f315f900dd6a636
zain srio <zein.srio@gmail.com>
2023-02-21 11:24:40 +0100

    CRUD task manager

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,55 @@
+class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  # def create
+  #   @task = Task.new(task_params)  #
+  #   if @task.save
+  #     redirect_to task_path(@task)
+  #   else
+  #     render :new
+  #   end
+  # end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
+    # else
+    #   render :edit
+    # end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+    # params.require(:name of the model).permit(:title, :details, :completed)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,19 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # read all
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show'
+
+  # create
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+  post '/tasks', to: 'tasks#create'
+  # read one
+  get '/tasks/:id', to: 'tasks#show', as: :task
+  # update
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch '/tasks/:id', to: 'tasks#update'
+  # delete
+  delete '/tasks/:id', to: 'tasks#destroy', as: :delete_task
 end
